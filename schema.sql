e356927d406ebb8214bd60c1e90066d10b5f84c0
Owusu-Desmond <desmond0406114009@gmail.com>
2022-09-15 18:06:19 +0000

    Create table for vets, visits and specializations

--- schema.sql
+++ schema.sql
@@ -37,3 +37,32 @@ ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 
 -- add a new column called owner_id to the animals table
 ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+
+
+
+-- Create a table named vets with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- name: string
+-- age: integer
+-- date_of_graduation: date
+-- There is a many-to-many relationship between the tables species and vets: a vet can specialize in multiple species, and a species can have multiple vets specialized in it. Create a "join table" called specializations to handle this relationship.
+-- There is a many-to-many relationship between the tables animals and vets: an animal can visit multiple vets and one vet can be visited by multiple animals. Create a "join table" called visits to handle this relationship, it should also keep track of the date of the visit.
+
+create table vets (
+    id int generated always as identity,
+    name varchar(255),
+    age int,
+    date_of_graduation date,
+    primary key (id)
+)
+
+create table specializations (
+    vet_id int references vets(id),
+    species_id int references species(id),
+)
+
+create table visits (
+    vet_id int references vets(id),
+    animal_id int references animals(id),
+    visit_date date,
+)
