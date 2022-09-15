8937166b633fdcf3a9bb485c9834ed491599b9d3
Owusu-Desmond <desmond0406114009@gmail.com>
2022-09-15 18:07:25 +0000

    Insert data into vets, specializations and visits table

--- data.sql
+++ data.sql
@@ -60,3 +60,41 @@ update animals set owner_id = 2 where name = 'Gabumon' or name = 'Pikachu';
 update animals set owner_id = 3 where name = 'Devimon' or name = 'Plantmon';
 update animals set owner_id = 4 where name = 'Charmander' or name = 'Squirtle' or name = 'Blossom';
 update animals set owner_id = 5 where name = 'Angemon' or name = 'Boarmon';
+
+-- data to insert into vet table
+insert into vets (name, age, date_of_graduation) values 
+('William Tatcher', 45, '2000-04-23'), 
+('Maisy Smith', 26, '2019-01-17'), 
+('Stephanie Mendez', 64, '1981-05-04'), 
+('Jack Harkness', 38, '2008-06-08')
+
+-- data to insertt into specializations table
+insert into specializations (vet_id, species_id) values 
+(1, 1), 
+(3, 1), 
+(3, 2), 
+(4, 2)
+
+-- data to insert into the visits table:
+insert into visits (vet_id, animal_id, visit_date) values 
+(1, 1, '2020-05-24'), 
+(3, 1, '2020-07-22'), 
+(4, 2, '2021-02-02'), 
+(2, 3, '2020-01-05'), 
+(2, 3, '2020-03-08'), 
+(2, 3, '2020-05-14'), 
+(3, 4, '2021-05-04'), 
+(4, 5, '2021-02-24'), 
+(2, 6, '2019-12-21'), 
+(1, 6, '2020-08-10'), 
+(2, 6, '2021-04-07'), 
+(3, 7, '2019-09-29'), 
+(4, 8, '2020-10-03'), 
+(4, 8, '2020-11-04'), 
+(2, 9, '2019-01-24'), 
+(2, 9, '2019-05-15'), 
+(2, 9, '2020-02-27'), 
+(2, 9, '2020-08-03'), 
+(3, 10, '2020-05-24'), 
+(1, 10, '2021-01-11')
+
